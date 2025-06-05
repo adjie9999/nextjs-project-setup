2e7d26ae0aa8e283e33595264500b27368e1611b
adjie9999 <hary.styaji@gmail.com>
2025-06-05 12:55:24 +0700

    Add .next/static/chunks/src_app_test_page_tsx_1b599af0._.js 1749102924952

---
+++ .next/static/chunks/src_app_test_page_tsx_1b599af0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f835ef40._.js",
+  "static/chunks/node_modules_0893d1bf._.js"
+],
+    source: "dynamic"
+});
